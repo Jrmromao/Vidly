1943c6f23515b3da6ce8ceeee532ba3bc2eb5187
Joao Romao <jrmromao@gmail.com>
2017-10-16 19:32:35 +0100

    Exercise 3

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,10 +1,17 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Customer
     {
         public int ID { get; set; }
+
+        [Required] //data annotations - overriding default conventions 
+        [StringLength(255)] //
         public string Name { get; set; }
 
-
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
